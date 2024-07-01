b49d1b725fc61f9deddd1ec8eb09af1f326d6d49
Dhruv-3241 <dhruv.25977@gmail.com>
2024-07-01 06:44:11 +0530

    Contract Address updated

--- frontend/src/pages/Wallet.jsx
+++ frontend/src/pages/Wallet.jsx
@@ -18,7 +18,7 @@ const Wallet = ({ saveState }) => {
         console.log(accounts);
         
         //The contract Address has also been stored in the FrontEnd portion in contract.js
-        const contractAddress = "0xc52cc9e7e91a93352671c3addf65e668ac766ac5";
+        const contractAddress = "0x91917ebcf580c07af8e1f3d8376194c85e676e9b";
         const contract = new web3.eth.Contract(ABI, contractAddress);
 
         // console.log(contract);
